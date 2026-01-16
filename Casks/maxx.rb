8f9977417a130f6a343844315bcdf58244521b0c
Awsl <awsl233777@gmail.com>
2026-01-16 20:32:32 +0800

    Update maxx.rb

--- Casks/maxx.rb
+++ Casks/maxx.rb
@@ -5,10 +5,10 @@ cask "maxx" do
   desc "maxx"
   if Hardware::CPU.intel?
     url "https://github.com/awsl-project/maxx/releases/download/v#{version}/maxx-macOS-amd64.dmg"
-    sha256 "ff0f3b0b2c43fffd1f8f4ad25c87f775b6b7b59fcd71676213344207c2714e8b"
+    sha256 "64d1b8003e6674132cd2d45f4bd36d8ec5859927728af3a9db3842f9d867e880"
   else
     url "https://github.com/awsl-project/maxx/releases/download/v#{version}/maxx-macOS-arm64.dmg"
-    sha256 "0a434604f9db2d4d3c10e0d1dabd5551d1f01325cc742ebf659bec9ed141e567"
+    sha256 "affde0548a72ce3fadf75c2894bd452d3cecf442b23406f06a4b3e3dc13c9554"
   end
 
   homepage "https://github.com/awsl-project/maxx"
