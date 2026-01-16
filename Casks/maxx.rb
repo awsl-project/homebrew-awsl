39abb5e73afb711c57c9e251d179dd48640d786c
Awsl <awsl233777@gmail.com>
2026-01-16 20:37:13 +0800

    Update maxx.rb

--- Casks/maxx.rb
+++ Casks/maxx.rb
@@ -8,7 +8,7 @@ cask "maxx" do
     sha256 "64d1b8003e6674132cd2d45f4bd36d8ec5859927728af3a9db3842f9d867e880"
   else
     url "https://github.com/awsl-project/maxx/releases/download/v#{version}/maxx-macOS-arm64.dmg"
-    sha256 "affde0548a72ce3fadf75c2894bd452d3cecf442b23406f06a4b3e3dc13c9554"
+    sha256 "sha256:942b54d0b29eb82708c7f96051a70491ae99723693836811dd58e0284bbcf777"
   end
 
   homepage "https://github.com/awsl-project/maxx"
